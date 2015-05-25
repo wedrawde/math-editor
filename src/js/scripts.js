da40850f69dd3ed7440138e0137a7dafd2ec6ade
Mika Väisänen <mika.vaisanen@reaktor.fi>
2015-05-25 16:55:51 +0300

    first day hacking, this branch will be depreciated

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,34 @@
 'use strict';
 
-var textBox = document.querySelector('textarea');
+$(document).ready(function() {
+	var textBox = document.querySelector('textarea');
+	textBox.focus();
 
+	var outputBox = document.querySelector('#output');
+	var input = textBox.value;
 
-var outputBox = document.querySelector('#output');
-var input = textBox.value;
+	function render (event) {
+		input = textBox.value;
+		outputBox.textContent = '`' + input + '`';
+		MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'output' ]);
+	}
 
-textBox.addEventListener('input', function (event) {
-	input = textBox.value;
-	outputBox.textContent = '`' + input + '`';
-	MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'output' ]);
+	textBox.addEventListener('input', render);
+
+	// var menubar = document.querySelectorAll('.syntax-tab > div');
+	// menubar.addEventListener('click', function (event) {
+	// 	console.log(event);
+	// 	console.log('click');
+	// });
+
+	$('.syntax-tab > div').on('click', function(event) {
+		var caretPos = textBox.selectionStart;
+		console.log(caretPos);
+		event.preventDefault();
+		textBox.focus();
+		var syntax = $(event.currentTarget).attr('id');
+		textBox.value += syntax;
+		render();
+
+	});
 });
-
